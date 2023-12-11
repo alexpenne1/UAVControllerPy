98c08e21d5d40c529d3c96c97acf102bb1f8642e
alexpenne1 <97004519+alexpenne1@users.noreply.github.com>
2023-12-11 16:36:39 -0500

    CHanged to pigpio

--- ESC.py
+++ ESC.py
@@ -1,8 +1,9 @@
 import logging
 import sys
 import time
-import RPi.GPIO as GPIO 
-
+import RPi.GPIO as GPIO
+import pigpio as pigpio
+import subprocess
 from Adafruit_BNO055 import BNO055
 
 def connectMotors(pins):
@@ -11,10 +12,15 @@ def connectMotors(pins):
     # Pins are as set on BOARD not by GPIO ordering.
 
     GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pins[0],GPIO.OUT)
+    GPIO.setup(pins[1],GPIO.OUT)
+    GPIO.setup(pins[2],GPIO.OUT)
+    GPIO.setup(pins[3],GPIO.OUT)
 
     # Set frequencies.
     motors = [GPIO.PWM(pins[0], 50), GPIO.PWM(pins[1], 50), GPIO.PWM(pins[2], 50), GPIO.PWM(pins[3], 50)]
+    #motors = [GPIO.PWM(pins[0], 50)]
     # Disconnect battery.
     input("Check that battery is disconnected from the base, then press enter.")
     # Set lowest PWM signal (4 for some reason).
@@ -27,7 +33,29 @@ def connectMotors(pins):
 
     return motors
     
+def connectMotorsPigpio(pins):
+    # Setup and connect motors.
+    print("Connecting motors...")
+    # Pins are as set on BOARD not by GPIO ordering.
     
+    mypi = pigpio.pi()
+    for pin in pins:
+        mypi.set_servo_pulsewidth(pin, 0)
+    
+    maxval = 1900
+    minval = 1100
+    
+    input("Check that battery is disconnected from the base, then press enter.")
+    for pin in pins:
+        mypi.set_servo_pulsewidth(pin, maxval)
+        
+    input("Connect the battery. Press enter when done.")
+    for pin in pins:
+        mypi.set_servo_pulsewidth(pin, minval)
+    print("4 beeps indicates drone is armed. Otherwise, needs callibration.")
+    print("End arming sequence.\n\n")
+    time.sleep(5)
+    return mypi
 
 
 
--- Main.py
+++ Main.py
@@ -7,7 +7,8 @@
 import logging
 import sys
 import time
-import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO
+import pigpio as pigpio
 from Adafruit_BNO055 import BNO055
 # Import functions from other files.
 # BNOSensor.py files.
@@ -16,33 +17,42 @@ from BNOSensor import callibrateSensor
 from BNOSensor import getStates
 # ESC.py files.
 from ESC import connectMotors
+from ESC import connectMotorsPigpio
 
-
+maxval = 1900
+minval = 1100
 # CONNECT AND CALLIBRATE
 # Raspberry Pi configuration with serial UART and RST connected to GPIO 18:
 # UART mode must be turned on (PS1 pin = HIGH).
 bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
 # Connect the sensor.
-connectSensor(bno)
+#connectSensor(bno)
 print("Sensor connected!")
 # Callibrate the sensor.
-callibrateSensor(bno)
+#callibrateSensor(bno)
 print("Sensor callibrated!\n\n")
 # Set pin numbers and connect the motors.
-pins = [18, 37, 11, 36]
-motors = connectMotors(pins)
+pins = [24, 26, 17, 16] # using GPIO.BCM numbering
+#motors = connectMotors(pins)
+mypi = connectMotorsPigpio(pins)
 print("Motors connected and callibrated!")
 
 
 # START PROGRAM
 print("Starting program. To kill drone, kill the program using Ctrl+C.")
 # Turn on motors.
-for motor in motors:
-    motor.ChangeDutyCycle(5.5)
-# Print yaw.
+
+for pin in pins:
+    mypi.set_servo_pulsewidth(pin, maxval)
 while True:
-    yaw, roll, pitch, w_x, w_y, w_z, a_x, a_y, a_z = getStates(bno)
-    print(yaw)
+    speed = input("enter speed: ")
+    for pin in pins:
+        mypi.set_servo_pulsewidth(pin, speed)
+
+# Print yaw.
+#while True:
+    #yaw, roll, pitch, w_x, w_y, w_z, a_x, a_y, a_z = getStates(bno)
+    #print(yaw)
 
 
 
