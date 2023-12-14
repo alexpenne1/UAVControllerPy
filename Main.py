33e830aa01936920066a536675b0ca4a7c533303
elocicer <ejl48@duke.edu>
2023-12-14 17:51:10 -0500

    made new sensor initialization function

--- Main.py
+++ Main.py
@@ -12,28 +12,14 @@ import RPi.GPIO as GPIO
 import pigpio as pigpio
 from Adafruit_BNO055 import BNO055
 # Import functions
+import Sensors
 import BNOSensor as BNO
 import Vicon
 import ESC
 import Controller as ctrl
 
-# CONNECT AND CALLIBRATE
-# Raspberry Pi configuration with serial UART and RST connected to GPIO 18:
-# UART mode must be turned on (PS1 pin = HIGH).
-bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
-# Connect the sensor.
-BNO.connectSensor(bno)
-print("Sensor connected!")
-# Callibrate the sensor.
-#BNO.callibrateSensor(bno)
-print("Sensor callibrated!\n\n")
-# Connect to Vicon
-vicon_client, mytracker = Vicon.connectVicon("192.168.0.101")
-object_name = "LoCicero_RPI_Drone"
+bno, mytracker, object_name = Sensors.init()
 
-# Get Object position
-#position = mytracker.get_position(object_name) # (latency, frame number, [[object_name,object_name,x,y,z,roll,pitch,yaw]]) (mm, rad)
-#print(f"Position: {position}")
 
 # Set pin numbers and connect the motors.
 pins = [24, 26, 17, 16] # using GPIO.BCM numbering
---
+++ Sensors.py
@@ -0,0 +1,18 @@
+import BNOSensor as BNO
+import Vicon
+
+def init():
+    # CONNECT AND CALLIBRATE
+    # Raspberry Pi configuration with serial UART and RST connected to GPIO 18:
+    # UART mode must be turned on (PS1 pin = HIGH).
+    bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
+    # Connect the sensor.
+    BNO.connectSensor(bno)
+    print("Sensor connected!")
+    # Callibrate the sensor.
+    #BNO.callibrateSensor(bno)
+    print("Sensor callibrated!\n\n")
+    # Connect to Vicon
+    vicon_client, mytracker = Vicon.connectVicon("192.168.0.101")
+    object_name = "LoCicero_RPI_Drone"
+    return bno, mytracker, object_name
