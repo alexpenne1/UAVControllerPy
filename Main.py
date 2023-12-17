3ace03a9eb6937ac9cdd84f4a39d95a8fd273e0a
elocicer <ejl48@duke.edu>
2023-12-17 13:50:13 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -24,14 +24,11 @@ bno, mytracker, object_name = Sensors.init(calibrate)
 pins, mypi = ESC.init()
 
 setpoint, state, cur_time, K, ue, vmax, rho, sigma, filter_states, filter_T, filter_K, yaw_looper, rawyaw = ctrl.init(bno, mytracker, object_name)
-print(f"Set Roll: {setpoint[3]}, Roll: {state[3]}, DRoll: {setpoint[3]-state[3]}, Set Pitch: {setpoint[4]}, Pitch: {state[4]}, DPitch: {setpoint[4]-state[4]}")
-# Controller loop.
+
 with open('data.csv', 'w', newline='') as myfile:
-    #csvwriter = csv.writer(myfile)
     try:
         while True:
             state, dx, cur_time, filter_states, yaw_looper, rawyaw = Sensors.getState(bno, mytracker, object_name, state, setpoint, cur_time, filter_states, filter_T, filter_K, yaw_looper,rawyaw)
-            print(f"Set Roll: {setpoint[3]}, Roll: {state[3]}, DRoll: {dx[3]}, Set Pitch: {setpoint[4]}, Pitch: {state[4]}, DPitch: {dx[4]}")
             inputs          = ctrl.CalculateControlAction_LQR(dx, K, ue, vmax, rho, sigma)
             ESC.writeMotors(mypi,pins,inputs)
             ctrl.SaveData(myfile, cur_time, state, inputs, dx)
