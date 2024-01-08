fc52f4824d7273cd361b32f566482d3a607cb40e
alexpenne1 <alexander.penne@duke.edu>
2024-01-08 16:58:17 +0000

    fixed index

--- Vicon.py
+++ Vicon.py
@@ -15,13 +15,18 @@ def connectVicon(VICON_TRACKER_IP):
     return vicon_client, mytracker
     #VICON_TRACKER_IP = "192.168.0.101"
 
-def GetLinearStates(tracker, object_name):
+def GetLinearStates(tracker, object_name, prev_state):
 # (latency, frame number, [[object_name,object_name,x,y,z,roll,pitch,yaw]]) (mm, rad)
-    positions = tracker.get_position(object_name)
-    # Extract x, y, z and return
-    x = positions[2][0][2]/1000
-    y = positions[2][0][3]/1000
-    z = positions[2][0][4]/1000
+    try:
+    	positions = tracker.get_position(object_name)
+    	# Extract x, y, z and return
+    	x = positions[2][0][2]/1000
+    	y = positions[2][0][3]/1000
+    	z = positions[2][0][4]/1000
+    except:
+	x = prev_state[0]
+	y = prev_state[1]
+	z = prev_state[2]
     return x, y, z
 
 
